d58ba2838146c19fe37d94ab3cc789d7b8550f65
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-11-10 11:02:31 -0600

    Update binary_gap.py

--- binary_gap.py
+++ binary_gap.py
@@ -1,34 +1,24 @@
-function dec2bin(dec){
-    return Number(dec).toString(2);
-} // function to convert a number to a binary value.
-
-let t=dec2bin(8);  let s=t.length; console.log("size string" + " " + s + " "+ "binary number :" + " " + t +"\n");
-// now get to code the gab between binary value generated 
-let deb = 0; // pour initialiser le compteur
-let max = 0; // qui se changera
-let maxv=0;
-if (t[deb]=="1")  // all number, binary starts with 1
-   console.log("start point");
-   let size = 1;
-   while(size < s )  // making a loop to navigate on other binary values of the number generated.
-        {
-          if(t[deb] != t[size])  //start the first binary value of the array , then size = deb + 1, si difference trouvee
-           {
-            // ici faut mettre le while
-             maxv = size-deb; // quand on a une difference, on fait une mise a jour du nombre de valeurs pour le gap
-             if(maxv > max)  // on compare cette valeur a la valeur maximale deja storee, on fait une mise a jour
-               {
-                 max = maxv;
-               }
-            }
-         else if(t[deb] == t[size]) //si egalite trouvee, on fait la mise a jour de la valeur ou on commence a faire descomparaisons
-            {
-             deb == size;  // pour controle
-             maxv = 0;
-            if(maxv > max)
-               {
-                 max = maxv;
-               }
-           }
-         size++;  
-        }
+function binarygap(nbre){
+ var nbrebin = Number(nbre).toString(2);
+ var size = nbrebin.length;
+alert("binary number : " + " " + nbrebin + " " + "taille : " + " " + size);
+var i = 0;
+var deb = 0;
+var gap = 0;
+var max = 0;
+ if(nbrebin[deb] == 1){
+    alert("debut comptage du gap");
+    while(i < size){
+      if(nbrebin[deb]!= nbrebin[i]){
+          gap = i - deb;
+          if(max < gap)
+              max = gap;
+      } // fin if
+      else
+         deb = i;
+     i++;
+    }//end while
+ } // end if
+console.log("binary gap : " + " " + max);
+}
+binarygap(17);
